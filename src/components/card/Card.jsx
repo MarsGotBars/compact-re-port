ebefc6dd8830038da0ad9da2a927b59bafe5622b
MarsGotBars <marcinm@live.nl>
2024-09-02 14:38:54 +0200

    Additions
    Setup some more css and card related stuff like the overall HTML structure and some planned stuff

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-import './card.css'
+import React from "react";
+import "./card.css";
 export default function Card() {
   return (
-    <main>Card</main>
-  )
+    <main>
+      <button>
+        see more
+      </button>
+      {/* functies toevoegen om over te switchen */}
+      <div>
+        <div>
+          <h1>Marcin</h1>
+          <h2>Developer</h2>
+        </div>
+        <figure>
+          <img src="" alt="" />
+        </figure>
+      </div>
+      <article>
+        {/* list with title? */}
+        <ul>
+          <li>Zwemmen</li>
+          <li>Golf</li>
+          <li>Gym</li>
+        </ul>
+      </article>
+      <div>
+        {/* this div should be hidden first */}
+        <h3>Code gerelateerde vaardigheden</h3>
+        {/* graphs or circles? */}
+      </div>
+    </main>
+  );
 }
